d99413e7366588ddc3e4a5548e6b48b8cff7bd53
soumit-s <soumit.srim@gmail.com>
2023-10-14 20:03:34 +0530

    Added new, delete and select all functionality to action bar. Also revamped the icons.

--- client/web/src/components/ActionBar.js
+++ client/web/src/components/ActionBar.js
@@ -2,28 +2,34 @@ import Add1 from "@/svg/Add1";
 import Delete1 from "@/svg/Delete1";
 import Filter1 from "@/svg/Filter1";
 
-export default function ActionBar({...props}) {
+import { PiTrash, PiFunnel, PiSelectionAll } from 'react-icons/pi'
+
+export default function ActionBar({ onNew, onDelete, onSelectAll, ...props }) {
     return (
         <div {...props} className="flex items-center justify-between">
             <div>
-                <button className="ok-button px-[0.7rem] flex items-center gap-[0.5em]">
+                <button className="ok-button px-[0.7rem] flex items-center gap-[0.5em]" onClick={e => onNew && onNew(e)}>
                     <Add1 className="w-6 h-6" />
                     <span>New</span>
                 </button>
             </div>
-            <div className="flex bg-white rounded border-[1px] border-zinc-300 overflow-hidden">
-                <Action>
-                    <Delete1 className="w-6 h-6" color='#222'/>
+            <div className="flex bg-white text-gray-700 rounded border-[1px] border-zinc-300 overflow-hidden">
+                <Action onClick={e => onSelectAll && onSelectAll(e)}>
+                    <PiSelectionAll className="text-xl" />
+                </Action>
+                <div className="flex-auto border-l-[1px] border-zinc-300"></div>
+                <Action onClick={e => onDelete && onDelete(e)}>
+                    <PiTrash className="text-xl" />
                 </Action>
                 <div className="flex-auto border-l-[1px] border-zinc-300"></div>
                 <Action>
-                    <Filter1 className="w-6 h-6" color='#222'/>
+                    <PiFunnel className="text-xl" />
                 </Action>
             </div>
         </div>
     )
 }
 
-function Action({children}) {
-    return <button className="p-2 hover:bg-zinc-100">{children}</button>
+function Action({ children, ...props }) {
+    return <button {...props} className="p-2 hover:bg-zinc-100">{children}</button>
 }
